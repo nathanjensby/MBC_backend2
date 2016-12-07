bddaf3594d7bc26158a843175e37e955c4f3210f
Nathan Jensby <njensby@gmail.com>
2016-12-07 15:26:33 -0600

    moved destroys around to help with seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Recipe.destroy_all
 Item.destroy_all
 Measurement.destroy_all
+Recipe.destroy_all
+
 
 #Recipes
 
